bdd9ed06cec97865d06c16a4c19833d666a15b73
vukute <86104313+vukute@users.noreply.github.com>
2023-04-04 21:53:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Hee luu cậu!',
-    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    introTitle: 'chaò bé Hoa !',
+    introDesc: `A có điều này muốn hỏi e, nhớ phải trả lời thật lòng nha`,
     btnIntro: 'oke',
-    title: 'Cậu yêu tớ có phải không 😙',
-    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là vợ tớ đó nha :> ',
+    title: 'e yêu có iu a phải không 😙',
+    desc: 'e không trả lời hoặc thoát ra, tức là có hahha :> ',
     btnYes: 'yêu ơi là iuuu <33',
-    btnNo: 'cậu mơ à :333',
-    question:'Nói cho tới lý do vì sao cậu yêu tớ đii <3',
-    btnReply: 'Gửi cho tớ <3',
-    reply: 'Tại vì cậu đẹp trai hihi <33333333',
-    mess: 'Tớ biết mà 🥰. Love you 300.000 😘😘',
-    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
+    btnNo: 'a mơ à :333',
+    question:'Nói cho tới lý do vì sao e mê a đê,
+    btnReply: 'Gửi cho a <3',
+    reply: 'Tại vì a đẹp trai vãi o` kakaakka <33333333',
+    mess: 'ohh, a biết mà 🥰. Love you 300.000 😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho a đi nào.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: https://www.facebook.com/luxy.mus26 //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
